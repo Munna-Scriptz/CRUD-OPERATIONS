84f1d4694c92e32fc71a7f0a4f3e118fbceb25dd
Munna <munnascriptz@gmail.com>
2026-02-11 18:39:58 +0600

    New CRUD_PROJECT

--- server.js
+++ server.js
@@ -1,13 +1,1 @@
-require("dotenv").config()
-const express = require("express")
-const router = require("./routes")
-const dbConfig = require("./dbConfig")
-const app = express()
-app.use(express.json())
-app.use(router)
-dbConfig()
-
-
-app.listen(8000, ()=>{
-    console.log("Server is running")
-})
+console.log("hello world")
