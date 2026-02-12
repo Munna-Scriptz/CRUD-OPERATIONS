a39ba86bc5aaff8e6c3c489c5148319340afaefd
Munna <munnascriptz@gmail.com>
2026-02-12 22:46:38 +0600

    Blog: New blog posting complete

--- controller/blogController.js
+++ controller/blogController.js
@@ -1,20 +1,41 @@
+const blogSchema = require('../models/blogSchema')
+
 const cloudinary = require('cloudinary').v2
 
 const create = async (req, res) => {
     try {
         const { slug, title, content, tags, isPublished } = req.body
+        const blogImg = req.file
+
+        // --------------------- validation
+        if (!blogImg) return res.status(400).send("Blog image is required")
+        if (!slug) return res.status(400).send("Blog slug is required")
+        if (!title) return res.status(400).send("Blog title is required")
+        if (!content) return res.status(400).send("Blog content is required")
 
         // --------------------- Upload to cloudinary 
-        
-        // --------------------- Upload to cloudinary 
-        const blogImg = req.file
         const base64String = blogImg.buffer.toString("base64")
         const dataUrl = `data:${blogImg.mimetype};base64,${base64String}`
         const cloudRes = await cloudinary.uploader.upload(dataUrl, { asset_folder: "CRUD_PROJECTS/Blog-API" })
-        console.log(cloudRes.secure_url)
+
+        // --------------------- Save to DB 
+        const newBlog = await blogSchema({
+            slug: slug.toLowerCase(),
+            title,
+            content,
+            image: cloudRes.secure_url,
+            tags,
+            isPublished
+        })
+
+        newBlog.save()
+
+        // --------------------- Success
+        res.status(201).send("New blog post published")
+
 
     } catch (error) {
-        console.log(error)
+        res.status(500).send("Internal server error")
     }
 }
 
